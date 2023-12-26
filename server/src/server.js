a7c17bccda068856da8a1ead4176daa6334104cd
Stepan Lavrenchuk <stepuntik@gmail.com>
2023-12-26 11:51:00 -0500

    add versioning

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
-const mongoose = require('mongoose');
+
+const { mongoConnect } = require('./services/mongo');
 
 const app = require('./app');
 
@@ -7,21 +8,10 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL =
-  'mongodb+srv://stepuntik:nasa-cluster@nasa-cluster.onqoafc.mongodb.net/?retryWrites=true&w=majority';
-
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
