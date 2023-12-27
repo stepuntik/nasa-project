dcf4a34b923e1868393d402f9b3437df8206f33c
Stepan Lavrenchuk <stepuntik@gmail.com>
2023-12-27 11:34:38 -0500

    security patch

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL =
-  'mongodb+srv://stepuntik:nasa-cluster@nasa-cluster.onqoafc.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
